8dbbe7ae22db37455e34ab0797b6b0a0fb1f5bde
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-04-30 20:10:06 +0300

    Initial

---
+++ 2 Sayı Arası Sayılarin Toplami/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim sayac, toplam As Integer
+        For sayac = TextBox1.Text To TextBox2.Text
+            toplam = toplam + sayac
+        Next
+        MsgBox(toplam)
+
+    End Sub
+End Class
---
+++ 2 Sayı Arası Sayılarin Toplami/WindowsApplication1/WindowsApplication1/Form2.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim a, toplam As Integer
+        For a = 1 To TextBox1.Text
+            toplam = toplam * a
+        Next
+        MsgBox(toplam)
+    End Sub
+End Class
