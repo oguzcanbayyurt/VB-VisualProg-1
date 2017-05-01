18e0cbe52c2f108fa54d2d45293f7b67c4be0166
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-01 21:49:12 +0300

    Initial

---
+++ Faktoriyel Hesaplama/WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim a, toplam As Integer
+        toplam = 1
+        For a = 1 To TextBox1.Text
+            toplam = toplam * a
+        Next
+        Label4.Text = toplam
+
+
+    End Sub
+End Class
