47f2c46c75874793592f0f28e6d621d9daed9654
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-01 21:46:48 +0300

    Initial

---
+++ Basit PictureBox Kullanimi/WindowsApplication8/WindowsApplication8/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        PictureBox2.ImageLocation = "Resources\1.jpg"
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs)
+        Me.BackColor = Color.Red
+    End Sub
+End Class
