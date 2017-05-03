0cfdc6861d139fecc3abda03f4e85a349f0968ab
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-03 22:01:32 +0300

    Initial

---
+++ Girilen 10 Sayının Ortalamasi/WindowsApplication7/WindowsApplication7/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim sayilar(9), a, toplam As Integer
+        toplam = 0
+        For a = 0 To 9
+            sayilar(a) = InputBox("Sayı Giriniz", a + 1 & ". Sayı", 55)
+            toplam = toplam + sayilar(a)
+        Next
+        MsgBox(toplam / sayilar.Length(), MsgBoxStyle.OkOnly, "Ortalama")
+    End Sub
+End Class
