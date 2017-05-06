7bdce40a6875425af8255220ca1b3d6e828ad70d
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-06 13:24:17 +0300

    Initial

---
+++ Inputbox - For ile Diziye Değer/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim dizi(9), a, toplam As Integer
+        For a = 0 To 9
+            dizi(a) = InputBox("sayı giriniz", a + 1 & ".sayı")
+            toplam = toplam + dizi(a)
+        Next
+        MsgBox(toplam / 10, MsgBoxStyle.OkOnly, "Ortalama")
+    End Sub
+End Class
