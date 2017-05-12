f6afbbbfd662395a2f021805814cdde82d20c050
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-13 00:21:55 +0300

    Initial

---
+++ Listbox'a Deger Aktarimi/WindowsApplication4/WindowsApplication4/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub ListBox1_DoubleClick(sender As Object, e As System.EventArgs) Handles ListBox1.DoubleClick
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If ListBox1.Items.Count < 10 Then
+            ListBox1.Items.Add(TextBox1.Text & MaskedTextBox1.Text)
+        Else
+            MsgBox("En Fazla 10 kişi kayıt edilebilir.Aktarmak için Aktar butonuna tıklayınız")
+            Button2.Visible = True
+
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form2.Show()
+    End Sub
+End Class
---
+++ Listbox'a Deger Aktarimi/WindowsApplication4/WindowsApplication4/Form2.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim kisiler(9) As Label
+        kisiler(0) = Label1
+        kisiler(1) = Label2
+        kisiler(2) = Label3
+        kisiler(3) = Label4
+        kisiler(4) = Label5
+        kisiler(5) = Label6
+        kisiler(6) = Label7
+        kisiler(7) = Label8
+        kisiler(8) = Label9
+        kisiler(9) = Label10
+        For i = 0 To Form1.ListBox1.Items.Count - 1
+            kisiler(i).Text = Form1.ListBox1.Items.Item(i)
+        Next
+    End Sub
+End Class
