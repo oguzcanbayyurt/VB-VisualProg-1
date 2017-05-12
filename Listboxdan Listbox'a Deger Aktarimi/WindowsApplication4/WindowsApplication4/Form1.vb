a1ad45e458a0e33aee7f44242476bb4055908a2a
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-13 00:23:45 +0300

    Initial

---
+++ Listboxdan Listbox'a Deger Aktarimi/WindowsApplication4/WindowsApplication4/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub ListBox1_DoubleClick(sender As Object, e As System.EventArgs) Handles ListBox1.DoubleClick
+        ListBox2.Items.Add(ListBox1.SelectedItem)
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub ListBox2_DoubleClick(sender As Object, e As System.EventArgs) Handles ListBox2.DoubleClick
+        ListBox1.Items.Add(ListBox2.SelectedItem)
+        ListBox2.Items.Remove(ListBox2.SelectedItem)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If ListBox2.Items.Count > 5 Then
+            MsgBox("5'den fazla ders seçemezsiniz")
+        Else
+        Me.Hide()
+        Form2.Show()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Listboxdan Listbox'a Deger Aktarimi/WindowsApplication4/WindowsApplication4/Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim dersler(4) As Label
+        dersler(0) = Label1
+        dersler(1) = Label2
+        dersler(2) = Label3
+        dersler(3) = Label4
+        dersler(4) = Label5
+        For i = 0 To Form1.ListBox2.Items.Count - 1
+            dersler(i).text = Form1.ListBox2.Items.Item(i)
+        Next
+    End Sub
+End Class
