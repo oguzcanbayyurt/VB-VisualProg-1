aae09b64b83c24bf1ce6a6723ef644c144c0b32f
Oğuzcan Bayyurt <oguzcan.bayyurt@gmail.com>
2017-05-21 01:06:34 +0300

    Initial

---
+++ Sayinin Faktoriyeli veya Kuvveti/WindowsApplication5/WindowsApplication5/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim a, toplam As Integer
+        toplam = 1
+        For a = 1 To TextBox1.Text
+            toplam = a * toplam
+        Next
+        MsgBox(toplam)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim kuvvet, toplam As Integer
+        kuvvet = InputBox("Hangi Sayının")
+        toplam = TextBox1.Text ^ kuvvet
+        MsgBox(toplam)
+    End Sub
+End Class
